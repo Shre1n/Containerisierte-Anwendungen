9b9d36be11a7b1c2fda94ac40630258c99c607f0
VNxyz1 <vincenzo_n@rocketmail.com>
2024-01-22 15:16:49 +0100

    removed test data

--- init.sql
+++ init.sql
@@ -23,11 +23,3 @@ CREATE TABLE user_module (
 ALTER TABLE user_module
 ADD FOREIGN KEY (user_id) REFERENCES user_table(id),
 ADD FOREIGN KEY (module_id) REFERENCES module(module_id) ON DELETE CASCADE;
-
-
-INSERT INTO module (module_id, module_name, crp, weight, grade) VALUES
-(1, "Naturwissenschaftliche und technische Grundlagen", 6, 1, 65),
-(2, "LA", 6, 1, 50),
-(3, "DM", 6, 1, 78),
-(4, "Algorithmen auf Sequenzen", 6, 2, 70),
-(5, "AUD", 6, 1, 90);
